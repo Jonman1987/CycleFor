6a9e06b772d92fb38c60e4f7fa3728d82e7d480d
Evgeny Danilov <Jonman@yandex.ru>
2023-09-17 15:59:18 +0700

    Task 4: Final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,21 +2,21 @@ package org.example;
 
 public class Main {
 
-    public static int multipleFour(int member) {
+    public static boolean multipleFour(int member) {
         if (member % 4 == 0) {
-            return member;
+            return true;
         }
-        return 0;
+        return false;
     }
 
     public static void main(String[] args) {
         int start = 1;
         int stop = 100;
 
-        int iterator;
-        for (iterator = stop; iterator > start; iterator--) {
-            if (multipleFour(iterator) != 0) {
-                System.out.println(iterator + " ");
+        int i;
+        for (i = stop; i > start; i--) {
+            if (multipleFour(i) == true) {
+                System.out.println(i + " ");
             }
         }
     }
